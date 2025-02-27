0b6f5fde303a9c2724e414ed33829ed851920566
Jean Carlo T. Celeste <jeancarlotarrobago.celeste@bicol-u.edu.ph>
2025-02-27 11:16:06 +0800

    initialize backend

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//Test
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
