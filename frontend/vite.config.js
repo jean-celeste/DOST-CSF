d39407b828fd85ed637b77be899ee7c0d2468911
Jean Carlo T. Celeste <jeancarlotarrobago.celeste@bicol-u.edu.ph>
2025-03-06 14:38:44 +0800

    Initialize frontend project structure with basic configuration and UI components. Shadcn

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,6 +10,11 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "./src"),
+    },
+  },
   server: {
     proxy: {
       '/api': {
