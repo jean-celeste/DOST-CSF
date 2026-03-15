c7b1283294b2208368cb1198b68006172f6bf891
kuri-kurimanju <guiraldescober.dost@gmail.com>
2026-03-16 06:01:51 +0800

    place holder

--- cc-charter/app/forms/page.tsx
+++ cc-charter/app/forms/page.tsx
@@ -1,7 +1,20 @@
+import Steps from "../../components/forms/Steps";
+
 export default function MainForm(){
     return (
         <>
-            Hello World
+            <div className="flex flex-row space-between justify-start min-h-screen py-2 px-4">
+                {/*Steps */}
+                    <div className="flex flex-col items-center justify-center lg-center w-1/3">
+                        <Steps />
+                    </div>
+                    <div className="flex flex-col items-center justify-center w-1/3">
+                        <Steps/>
+                    </div>
+                    <div className="flex flex-col items-center justify-center w-1/3">
+                        <Steps/>
+                    </div>
+            </div>
         </>
     )
 }
---
+++ cc-charter/components/forms/Steps.tsx
@@ -0,0 +1,14 @@
+export default function Steps(){
+    return (
+        <>
+            <div className="flex flex-col items-center width-full justify-center">
+                <div className="flex flex-col items-center justify-center width-1/3">
+                    <img src="step-1 eme" alt="Personal Details"></img>
+                    <img src="step-2 eme" alt="Citizens Charter"></img>
+                    <img src="step-3 eme" alt="Suggestions"></img>
+                    <img src="step-4 eme" alt="Review"></img>
+                </div>
+            </div>
+        </>
+    )
+}
